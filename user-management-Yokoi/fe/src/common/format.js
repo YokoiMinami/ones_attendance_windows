1f5083ebcea6e372dd6b0af063aa3628f381981f
YokoiMinami <ones.minami.yokoi@gmail.com>
2024-12-05 12:47:11 +0900

    DigitalClockを分割

--- user-management-Yokoi/fe/src/common/format.js
+++ user-management-Yokoi/fe/src/common/format.js
@@ -6,3 +6,21 @@ export const formatDate = (dateString) => {
   const day = String(date.getDate()).padStart(2, '0');
   return `${year}/${month}/${day}`;
 };
+
+// 日付、曜日フォーマット
+export const formatTopDate = (now) => {
+  const weekday = ['日', '月', '火', '水', '木', '金', '土'];
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1;
+  const day = now.getDate();
+  const dayOfWeek = weekday[now.getDay()];
+  return `${year}/${month}/${day} ${dayOfWeek}`;
+};
+
+//時間フォーマット
+export const formatTime = (now) => {
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+  return `${hours}:${minutes}:${seconds}`;
+};
